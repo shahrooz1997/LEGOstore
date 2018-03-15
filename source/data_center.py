1fd3bb3be855846f84a546e39e2505ca47040f36
Ubuntu <ubuntu@ip-172-31-42-127.us-west-2.compute.internal>
2018-03-15 02:14:59 +0000

    Adding config file

---
+++ source/data_center.py
@@ -0,0 +1,12 @@
+class data_center:
+    # Each data center wil store the information about the servers it has and also periodically checks the health of servers in it.
+    def __init__():
+        server_list
+
+    def add_server():
+
+    def remove_server():
+
+    # Get the MC Router relevant to the server
+    def get_mc_router():
+        

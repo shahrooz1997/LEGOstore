3fd829efccd725e625818cb341260011dce77da2
Chetan Sharma <chetansharma@net19pc182.cse.psu.edu>
2018-06-21 01:18:48 -0400

    Database populating scriot

---
+++ source/populate_database.py
@@ -0,0 +1,52 @@
+import json
+from Client import Client
+
+import threading
+import random
+import logging
+import string
+from datetime import datetime
+
+
+def get_logger(log_path):
+    logger_ = logging.getLogger('log')
+    logger_.setLevel(logging.INFO)
+    handler = logging.FileHandler(log_path)
+    # XXX: TODO: Check if needed
+    handler.setFormatter(logging.Formatter('%(message)s'))
+    logger_.addHandler(handler)
+
+
+def thread_wrapper(output_logger, start_index, end_index, value_size=100000):
+    # We will assume fixed class for wrapper for now
+
+    for i in range(start_index, end_index):
+        value = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                        for _ in range(value_size))
+        output_logger.info(json.dumps(client.insert("key"+str(i), value, "Viveck")))
+
+    return
+
+
+if __name__ == "__main__":
+    properties = json.load(open('client_config.json'))
+    client_id = "1"
+    client = Client(properties, client_id)
+    output_logger = get_logger("insert.log")
+
+    keys_to_write = 110000
+    start_index = keys_to_write * (int(client_id) - 1)
+    end_index = start_index + keys_to_write
+    number_of_thread = 20
+
+    thread_list = []
+    for i in range(0, number_of_thread):
+        # Please not we assume client id starts from 1
+        s_index = start_index + i * (keys_to_write/number_of_thread)
+        e_index = start_index + (i + 1) * (keys_to_write/number_of_thread)
+
+        thread_list.append(threading.Thread(target=thread_wrapper, args=(output_logger, s_index, e_index)))
+        thread_list[-1].start()
+
+    for i in range(0, number_of_thread):
+        thread_list[i].join()

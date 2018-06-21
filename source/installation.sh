147aab304d511752a6595c6a6ee6119ebb168927
Ubuntu <ubuntu@ip-172-31-31-133.ap-south-1.compute.internal>
2018-06-21 18:02:35 +0000

    Installation script for python3

--- source/installation.sh
+++ source/installation.sh
@@ -2,6 +2,9 @@ sudo apt-get update
 sudo apt install libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev bzip2 build-essential gcc -y
 git clone https://github.com/facebook/rocksdb.git /tmp/rocksdb
 cd /tmp/rocksdb
+git checkout v4.5.1
+echo "Compiling RocksDB... This will take a while."
+make shared_lib -j 2
 sudo make install-shared INSTALL_PATH=/usr
 sudo apt install python3-pip
 pip3 install pyrocksdb
@@ -13,8 +16,8 @@ sudo python3 setup.py install
 cd
 git clone https://github.com/openstack/pyeclib.git
 cd pyeclib
-sudo apt-get install build-essential python-dev python-pip liberasurecode-dev
-sudo pip install -U bindep -r test-requirements.txt
+sudo apt-get install build-essential python-dev python-pip python3-pip liberasurecode-dev -y
+sudo pip3 install -U bindep -r test-requirements.txt
 sudo python3 setup.py install
-sudo echo ‘include /usr/local/lib’ >> /etc/ld.so.conf
+sudo sh -c "echo '/usr/local/lib' >> /etc/ld.so.conf"
 sudo ldconfig

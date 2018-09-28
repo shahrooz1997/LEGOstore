f2da3fe4a16331c892bc349e8bfbd393e9d622fc
Nader <nna5040.psu@gmail.com>
2018-09-28 03:58:50 +0000

    fix clean.py bug

--- source/clean.py
+++ source/clean.py
@@ -1,5 +1,5 @@
 import os
-os.system("rm output_* individual_times_1* socket_times_* coding_times_* timestamp_result.json")
+os.system("rm output_* individual_times_* socket_times_* coding_times_* timestamp_result.json")
 ans = input("delete merged files? y/n ")
 if ans == 'y':
 	os.system("rm 1_*")

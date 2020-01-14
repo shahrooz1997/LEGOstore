ae3cd1eaf606160fac50de57df120cd52a146fbb
Hamidreza <shahrooz.1000@gmail.com>
2020-01-14 13:29:10 -0500

    Timestamp implemented

---
+++ inc/Timestamp.h
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Timestamp.h
+ * Author: shahrooz
+ *
+ * Created on January 4, 2020, 11:35 PM
+ */
+
+#ifndef Timestamp_H
+#define Timestamp_H
+
+#include <string>
+#include <stdint.h>
+#include <vector>
+#include <assert.h>
+
+class Timestamp {
+public:
+    Timestamp(uint32_t client_id);
+    std::string get_string();
+    Timestamp& max_timestamp(std::vector<Timestamp*>& v);
+    Timestamp increase_timestamp(const Timestamp& timestamp, const uint32_t client_id);
+    virtual ~Timestamp();
+
+private:
+	uint32_t time;
+	uint32_t client_id;
+};
+
+#endif /* Timestamp_H */
+
---
+++ src/Timestamp.cpp
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Timestamp.cpp
+ * Author: shahrooz
+ * 
+ * Created on January 4, 2020, 11:35 PM
+ */
+
+#include "Timestamp.h"
+
+Timestamp::Timestamp(uint32_t client_id){
+	this->client_id = client_id;
+	time = 0;
+}
+
+std::string Timestamp::get_string(){
+	return std::to_string(time) + '-' + std::to_string(client_id);
+}
+
+Timestamp& Timestamp::max_timestamp(std::vector<Timestamp*>& v){
+	assert(!v.empty());
+
+	std::vector<Timestamp*>::iterator it_max = v.begin();
+	for(std::vector<Timestamp*>::iterator it = v.begin(); it != v.end(); it++){
+		if((*it)->time > (*it_max)->time ||
+			((*it)->time == (*it_max)->time && (*it)->client_id < (*it_max)->client_id)){
+			it_max = it;
+		}
+	}
+
+	return *(*it_max);
+}
+
+Timestamp Timestamp::increase_timestamp(const Timestamp& timestamp, const uint32_t client_id){
+	Timestamp ret(client_id);
+	ret.time = timestamp.time + 1;
+	return ret;
+}
+
+Timestamp::~Timestamp(){
+
+}

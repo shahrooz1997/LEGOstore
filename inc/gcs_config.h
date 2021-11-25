e394ada1401ad203b8db8c0bdd1ec849faeccc99
Rajveer Bachkaniwala <rajveerbachkaniwala29@gmail.com>
2021-11-25 14:43:17 -0500

    shifted GCS config to gcs_config.h

---
+++ inc/gcs_config.h
@@ -0,0 +1,11 @@
+// Uncomment to enable GCS
+#define GCS
+
+#ifdef GCS
+
+    // Note below can be configured to be datacenter specific
+    // TODO: Rajveer
+    #define GCS_PROJECT_ID "personal-use-316018"
+    #define GCS_STORAGE_LOCATION "us-east1"
+    
+#endif
